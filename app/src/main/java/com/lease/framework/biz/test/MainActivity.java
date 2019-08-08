49fac59bda12cdca33ecb4f5e53c2fc8894ad9ef
lhlhlh111000 <zlh111000@gmail.com>
2019-08-08 10:57:33 +0800

    add: 新增权限申请库

--- app/src/main/java/com/lease/framework/biz/test/MainActivity.java
+++ app/src/main/java/com/lease/framework/biz/test/MainActivity.java
@@ -5,6 +5,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.lease.framework.core.ToastUtils;
+import com.yanzhenjie.permission.Action;
+import com.yanzhenjie.permission.AndPermission;
+import com.yanzhenjie.permission.runtime.Permission;
+
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -24,5 +31,35 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, NetworkTestActivity.class));
             }
         });
+        findViewById(R.id.btn_permission).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                testPermission();
+            }
+        });
+    }
+
+    private void testPermission() {
+        AndPermission.with(this)
+                .runtime()
+                .permission(Permission.Group.CAMERA)
+                .rationale(new RuntimeRationale())
+                .onGranted(new Action<List<String>>() {
+                    @Override
+                    public void onAction(List<String> data) {
+                        ToastUtils.showToast(MainActivity.this, "允许");
+                    }
+                })
+                .onDenied(new Action<List<String>>() {
+                    @Override
+                    public void onAction(List<String> data) {
+                        if(AndPermission.hasAlwaysDeniedPermission(MainActivity.this, data)) {
+                            AndPermission.with(MainActivity.this).runtime().setting().start(1000);
+                        }else {
+                            ToastUtils.showToast(MainActivity.this, "拒绝");
+                        }
+                    }
+                })
+                .start();
     }
 }
