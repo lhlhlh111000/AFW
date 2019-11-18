49b45ad05c10c4e2ffb51e7d3cc3821caad6238c
lhlhlh111000 <zlh111000@gmail.com>
2019-11-18 14:53:09 +0800

    fix: 响应数据为空的情况处理

--- network/src/main/java/com/lease/framework/network/DefaultNetworkConfig.java
+++ network/src/main/java/com/lease/framework/network/DefaultNetworkConfig.java
@@ -1,5 +1,22 @@
 package com.lease.framework.network;
 
+import android.text.TextUtils;
+
+import com.google.gson.reflect.TypeToken;
+
+import java.io.IOException;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Type;
+import java.nio.charset.Charset;
+
+import okhttp3.RequestBody;
+import okhttp3.ResponseBody;
+import okio.Buffer;
+import okio.BufferedSource;
+import retrofit2.Converter;
+import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -9,7 +26,60 @@ public class DefaultNetworkConfig extends NetworkConfig {
 
     public DefaultNetworkConfig() {
         setAdapterFactory(RxJava2CallAdapterFactory.create());
-        setConverterFactory(GsonConverterFactory.create());
+        setConverterFactory(EmptyConverterFactory.create());
         setTimeout(DEFAULT_TIMEOUT);
+    }
+
+    static class EmptyConverterFactory extends Converter.Factory {
+
+        private GsonConverterFactory factory;
+
+        public static EmptyConverterFactory create() {
+            return new EmptyConverterFactory(GsonConverterFactory.create());
+        }
+
+        private EmptyConverterFactory(GsonConverterFactory factory) {
+            this.factory = factory;
+        }
+
+        @Override
+        public Converter<?, RequestBody> requestBodyConverter(Type type, Annotation[] parameterAnnotations, Annotation[] methodAnnotations, Retrofit retrofit) {
+            return factory.requestBodyConverter(type, parameterAnnotations, methodAnnotations, retrofit);
+        }
+
+        @Override
+        public Converter<ResponseBody, ?> responseBodyConverter(final Type type, Annotation[] annotations, Retrofit retrofit) {
+            final Converter<ResponseBody, ?> delegate = factory.responseBodyConverter(type, annotations, retrofit);
+            return new Converter<ResponseBody,Object>() {
+                @Override
+                public Object convert(ResponseBody body) throws IOException {
+                    BufferedSource source = body.source();
+                    source.request(Long.MAX_VALUE); // request the entire body.
+                    Buffer buffer = source.buffer();
+                    String responseStr = buffer.clone().readString(Charset.forName("UTF-8"));
+                    if (TextUtils.isEmpty(responseStr)) {
+                        try {
+                            Constructor[] constructors = Class.forName(TypeToken.get(type).getRawType().getName()).getDeclaredConstructors();
+                            for(Constructor constructor : constructors) {
+                                try {
+                                    constructor.setAccessible(true);
+                                    return constructor.newInstance();
+                                }catch (IllegalAccessException e) {
+                                    e.printStackTrace();
+                                } catch (InstantiationException e) {
+                                    e.printStackTrace();
+                                } catch (InvocationTargetException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        }catch (ClassNotFoundException e) {
+                            e.printStackTrace();
+                        }
+                        return null;
+                    }
+                    return delegate.convert(ResponseBody.create(body.contentType(), responseStr));
+                }
+            };
+        }
     }
 }
